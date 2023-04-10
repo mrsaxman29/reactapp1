d399364444aea8721f9f4181a2a963e799d3b710
Daniel Mensch <danmensch@gmail.com>
2023-04-09 23:48:46 -0400

    added video props3

--- reactapp1/src/Project_Tile.js
+++ reactapp1/src/Project_Tile.js
@@ -22,13 +22,16 @@ export default function Project_Tile(props) {
     <div className='tile_wrapper'>
         <div className='video_wrapper'>
             <video className='video' muted autoPlay loop src={video} />
-            <button className='linkbuttons'>CODE</button>
-            <button className='linkbuttons'>PROJECT</button>
+            <div className='button_wrapper'>
+                <a href={code_url} target="_blank" className='linkbuttons'>CODE &#9999;</a>
+                <a href={deployed_url} target="_blank" className='linkbuttons'>PROJECT</a>
+            </div>
+            
             
         </div>
         <div className='text_wrapper'>
             <div className="title">{title}</div>
-            <div className='summary'>SUMMARY:   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+            <div className='summary'><p>{summary}</p></div>
             <div  className='tags_wrapper'>TOPIC TAGS: 
                 {spellitaire_tags.map((tag)=>(
 
