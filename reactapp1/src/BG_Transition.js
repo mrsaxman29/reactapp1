bdb9b9c5e470e60a8fabe35bbe6ce65edfb65991
Daniel Mensch <danmensch@gmail.com>
2023-04-12 11:59:07 -0400

    yay

--- reactapp1/src/BG_Transition.js
+++ reactapp1/src/BG_Transition.js
@@ -11,6 +11,13 @@ export default function BG_Transition() {
       <div class="top_color">
         <h1 id="headline">CREATING THINGS WITH CODE AND SHARING THEM WITH THE WORLD</h1>
       </div>
+
+      <div id='git_wrapper'>
+        <h2>My Live GitHub Contributions</h2>
+        <img id='git' src="http://ghchart.rshah.org/mrsaxman29" alt="2016rshah's Github chart" />
+
+      </div>
+      
       
       <img id="svg" src='../bg2.svg'></img>
       <SVG1></SVG1>
